e5b9de832c36338d7d2f21ef86726f2f4470f4ea
patbytes <patbytes256@gmail.com>
2021-08-24 19:49:34 -0500

    Initial commit

---
+++ app/src/main/java/edu/mssu/cis385/helloworld1/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.mssu.cis385.helloworld1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+    }
+}
