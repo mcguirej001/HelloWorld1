ce1c474fd1b553b1910ee45d965bab242cb50874
patbytes <patbytes256@gmail.com>
2021-08-24 20:03:04 -0500

    Adding test comment

--- app/src/main/java/edu/mssu/cis385/helloworld1/MainActivity.java
+++ app/src/main/java/edu/mssu/cis385/helloworld1/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Hello World");
+        //testing git commit
     }
 }
